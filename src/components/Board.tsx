971991a151bca0b94417812dfa97cc4da633782a
emanuelefavero <emafaux89@gmail.com>
2024-02-22 07:27:59 +0100

    add components/Square

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,16 +1,18 @@
+import Square from './Square'
+
 export default function Board() {
   return (
     <>
       <div className='board'>
-        <button className='square'>1</button>
-        <button className='square'>2</button>
-        <button className='square'>3</button>
-        <button className='square'>4</button>
-        <button className='square'>5</button>
-        <button className='square'>6</button>
-        <button className='square'>7</button>
-        <button className='square'>8</button>
-        <button className='square'>9</button>
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+        <Square />
       </div>
     </>
   )
---
+++ src/components/Square.tsx
@@ -0,0 +1,7 @@
+export default function Square() {
+  return (
+    <>
+      <button className='square'>1</button>
+    </>
+  )
+}
