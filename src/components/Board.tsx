1e7336ed0f4a778de4e7c2fc6c5c42599184b005
emanuelefavero <emafaux89@gmail.com>
2024-02-22 08:08:06 +0100

    refactor components/Board

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -3,16 +3,17 @@ import Square from './Square'
 
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null))
-  const [turn, setTurn] = useState('X')
+  const [xIsNext, setXIsNext] = useState(true)
 
   const handleClick = (value: string | null, index: number) => {
     if (value) return // if the square is already filled, return early
 
+    // update the square with the current player value
     const nextSquares = [...squares]
-    nextSquares[index] = turn
+    nextSquares[index] = xIsNext ? 'X' : 'O'
     setSquares(nextSquares)
 
-    setTurn(turn === 'X' ? 'O' : 'X')
+    setXIsNext(!xIsNext) // toggle the next player
   }
 
   return (
