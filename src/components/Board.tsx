73d9018ce0cfabd76cd7bf3c45199a85ea566739
emanuelefavero <emafaux89@gmail.com>
2024-02-22 08:01:13 +0100

    add X and O click

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,18 +1,30 @@
+import { useState } from 'react'
 import Square from './Square'
 
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null))
+  const [turn, setTurn] = useState('X')
+
+  const handleClick = (value: string | null, index: number) => {
+    if (value) return // if the square is already filled, return early
+
+    const nextSquares = [...squares]
+    nextSquares[index] = turn
+    setSquares(nextSquares)
+
+    setTurn(turn === 'X' ? 'O' : 'X')
+  }
+
   return (
     <>
       <div className='board'>
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
+        {squares.map((value, index) => (
+          <Square
+            key={index}
+            value={value}
+            onSquareClick={() => handleClick(value, index)}
+          />
+        ))}
       </div>
     </>
   )
--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -1,15 +1,12 @@
-import { useState } from 'react'
+interface Props {
+  value: string
+  onSquareClick: () => void
+}
 
-export default function Square() {
-  const [value, setValue] = useState('')
-
-  const handleClick = () => {
-    setValue('X')
-  }
-
+export default function Square({ value, onSquareClick }: Props) {
   return (
     <>
-      <button className='square' onClick={handleClick}>
+      <button className='square' onClick={onSquareClick}>
         {value}
       </button>
     </>
